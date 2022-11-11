9e983e465561642bd028919f8914334d1217b3b1
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-11 13:06:01 +0700

    fix: Изменить переменную

--- frontend/src/utils/duckAuth.js
+++ frontend/src/utils/duckAuth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.API_URL || 'https://auth.nomoreparties.co'; // базовая ссылка
+export const BASE_URL = process.env.REACT_APP_API_URL || 'https://auth.nomoreparties.co'; // базовая ссылка
 
 export function request({url,method = 'POST',token,data}) {
   return fetch(`${BASE_URL}${url}`, {
