adb3725b03e83ac7a9f1842cc2cd7bc4a8dba4c9
Ekaterina Vodyankina <ekaterina.lopandina@yandex.ru>
2022-11-11 17:41:34 +0700

    feat: Добавить переменные окружения

--- backend/constants.js
+++ backend/constants.js
@@ -1,4 +1,8 @@
-const JWT_SECRET = '4710391895964416554ee9f99324e94fa6e1629adefda7a981491ce871830290';
+const {
+  NODE_ENV,
+  PORT = 3000,
+  JWT_SECRET,
+} = process.env;
 const MONGO_CODE = 11000;
 const REGEX_URL = /^https?:\/\/(www\.)?[\w\d-]+\.\w/;
 
@@ -6,4 +10,6 @@ module.exports = {
   JWT_SECRET,
   MONGO_CODE,
   REGEX_URL,
+  PORT,
+  NODE_ENV,
 };
